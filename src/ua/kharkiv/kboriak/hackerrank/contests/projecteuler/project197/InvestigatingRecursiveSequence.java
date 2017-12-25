8382bb310f164af865abdd11072636a3511c550d
kboriak <kostiantyn.boriak@gmail.com>
2017-12-25 12:18:56 +0200

    Project Euler #197 solved

--- src/ua/kharkiv/kboriak/hackerrank/contests/projecteuler/project197/InvestigatingRecursiveSequence.java
+++ src/ua/kharkiv/kboriak/hackerrank/contests/projecteuler/project197/InvestigatingRecursiveSequence.java
@@ -1,25 +1,34 @@
 package ua.kharkiv.kboriak.hackerrank.contests.projecteuler.project197;
 
-import ua.kharkiv.kboriak.utils.annotations.Unsolved;
-
 import java.util.Scanner;
 
 // https://www.hackerrank.com/contests/projecteuler/challenges/euler197/problem
-@Unsolved
 public class InvestigatingRecursiveSequence {
 
+    private static double sumOfTwoLastElementsInSequence(double u0, double b, double error) {
+        double u1 = Double.MIN_VALUE;
+        double u2 = u0;
+        double temp;
+        do {
+            temp = u1;
+            u1 = u2;
+            u2 = nextInSequence(u1, b);
+            if (temp == u2) {
+                break;
+            }
+        } while (Math.abs(u2 - u1) > error);
+        return u1 + u2;
+    }
+
+    private static double nextInSequence(double x, double b) {
+        return Math.floor(Math.pow(2, b - x * x)) * 0.000000001;
+    }
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in).useDelimiter("\\s+");
+        Scanner scanner = new Scanner(System.in);
         double u0 = scanner.nextDouble();
         double b = scanner.nextDouble();
         scanner.close();
-        double power = Math.pow(10, -9);
-        double u1;
-        double u2 = u0;
-        do {
-            u1 = u2;
-            u2 = Math.floor(Math.pow(2, b - u1 * u1)) * power;
-        } while(Math.abs(u2 - u1) > power * 2);
-        System.out.printf("%.9f", u2 + u1);
+        System.out.printf("%.9f", sumOfTwoLastElementsInSequence(u0, b, Math.pow(10, -8)));
     }
 }
